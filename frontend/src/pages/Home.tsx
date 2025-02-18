623195681a7c102764a212c3e85dc0e22db1bf3b
Axel Velasquez <yoloax11@gmail.com>
2025-02-18 00:05:26 -0800

    homepage component and schedule page --lot to do--

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import Scheduler from "../components/Scheduler"
+export default function Home(){
+
+    return(
+        <div> 
+            <h1>Homepage</h1>
+            <Scheduler />
+        </div>
+    )
+}
