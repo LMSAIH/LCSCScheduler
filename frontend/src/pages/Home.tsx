3a7068dbe3e08938b68541c0e9b5532fc151a917
Axel Velasquez <yoloax11@gmail.com>
2025-02-18 20:26:19 -0800

    added some spice

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,25 @@
 import Scheduler from "../components/Scheduler"
-export default function Home(){
+import AdminScheduler from "../components/AdminScheduler"
+export default function Home() {
 
-    return(
-        <div> 
-            <h1>Homepage</h1>
-            <Scheduler />
+    return (
+
+        <div className="min-h-screen bg-[#121212] text-white">
+            <main className="mx-auto p-6">
+                <Scheduler />
+                <div className="mt-8 flex justify-center">
+                    <button
+                        className="px-6 py-2 bg-[#F15A29] hover:bg-[#D14918] 
+                       rounded-md transition-colors font-medium"
+                    >
+                        Send Schedule
+                    </button>
+                </div>
+
+                <AdminScheduler />
+                
+            </main>
         </div>
+
     )
 }
