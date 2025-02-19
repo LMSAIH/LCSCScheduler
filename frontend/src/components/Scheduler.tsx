5fed24c05cabc045f1a3f7956f31488aaf3fa50b
Axel Velasquez <yoloax11@gmail.com>
2025-02-18 20:26:06 -0800

    modified the scheduler to account for permanent objects

--- frontend/src/components/Scheduler.tsx
+++ frontend/src/components/Scheduler.tsx
@@ -5,40 +5,42 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
 type Event = {
-    id: string,
-    title: string,
-    start: Date,
-    end: Date,
+    id: string;
+    title: string;
+    start?: Date;
+    end?: Date;
+    daysOfWeek?: number[];
+    startTime?: string;
+    endTime?: string;
     color: string;
+    type: string;
 };
 
+
 export default function Scheduler() {
-    const [events, setEvents] = useState<Event[] | []>([]);
+    const [events, setEvents] = useState<Event[]>([]);
     const [objectState, setObjectState] = useState<"Permanent" | "Temporary">("Permanent");
     const [id, setId] = useState(1);
 
     const handleEventClick = (info: any) => {
         if (window.confirm(`Delete event "${info.event.title}"?`)) {
+
             setEvents(events.filter(event => event.id !== info.event.id));
         }
-
     };
 
     const handleDateSelect = (info: any) => {
-
         if (!info) {
             return;
         }
 
-        let eventStart, eventEnd;
+        let eventStart: Date, eventEnd: Date;
 
         if (info.allDay) {
-
             eventStart = new Date(info.start);
             eventStart.setHours(7, 0, 0, 0);
             eventEnd = new Date(info.start);
             eventEnd.setHours(19, 0, 0, 0);
-            console.log(eventStart, eventEnd)
         } else {
             if (info.start.getDay() !== info.end.getDay()) {
                 document.body.style.cursor = "not-allowed";
@@ -51,50 +53,77 @@ export default function Scheduler() {
             eventEnd = info.end;
         }
 
-        const newEvent = {
-            id: String(id),
-            title: objectState === "Permanent" ? "Permanent" : "Temporary",
-            start: eventStart,
-            end: eventEnd,
-            color: objectState === "Permanent" ? "green" : "yellow",
-        };
-        setEvents([...events, newEvent]);
-        setId(id + 1);
+        let newEvent: Event;
+        if (objectState === "Permanent") {
+            newEvent = {
+                id: String(id),
+                title: "Permanent",
+                daysOfWeek: [eventStart.getDay()],
+                startTime: eventStart.toTimeString().slice(0, 8),
+                endTime: eventEnd.toTimeString().slice(0, 8),
+                color: "green",
+                type: objectState,
+            };
+        } else {
+            newEvent = {
+                id: String(id),
+                title: "Temporary",
+                start: eventStart,
+                end: eventEnd,
+                color: "yellow",
+                type: objectState,
+            };
+        }
 
+        setEvents([...events, newEvent]);
+
+        console.log(events)
+        setId(id + 1);
     };
 
     const handleEventDrop = (info: any) => {
-
         if (info.event.allDay) {
             info.revert();
             return;
         }
-
         const updatedEvents = events.map((event) => {
+
+
             if (event.id === info.event.id) {
-                return {
-                    ...event,
-                    start: info.event.start,
-                    end: info.event.end,
-                };
+
+                if (event.type === "Temporary") {
+                    return {
+                        ...event,
+                        start: info.event.start,
+                        end: info.event.end,
+                    };
+                } else {
+                    
+                    return {
+                        ...event,
+                        daysOfWeek: [info.event.start.getDay()],
+                        startTime: info.event.start.toTimeString().slice(0,8),
+                        endTime: info.event.end.toTimeString().slice(0,8)
+                    }
+                }
             }
             return event;
         });
-
         setEvents(updatedEvents);
     };
+
     return (
         <div className="w-1/2 m-auto">
             <div className="flex flex-row gap-10 justify-center mb-5">
                 <button
                     onClick={() => setObjectState("Permanent")}
-                    className={`${objectState === "Permanent" ? "bg-green-500" : "bg-gray-200"} hover:cursor-pointer rounded-md p-4`}
+                    className={`${objectState === "Permanent" ? "bg-[#F15A29]" : "bg-gray-500"} hover:cursor-pointer rounded-md px-6 py-2`}
                 >
                     Permanent Object
                 </button>
                 <button
                     onClick={() => setObjectState("Temporary")}
-                    className={`${objectState === "Temporary" ? "bg-green-500" : "bg-gray-200"} hover:cursor-pointer rounded-md p-4`}
+                    className={`${objectState === "Temporary" ? "bg-[#F15A29]" : "bg-gray-500"} hover:cursor-pointer rounded-md px-6 py-2`}
                 >
                     Temporary Object
                 </button>
