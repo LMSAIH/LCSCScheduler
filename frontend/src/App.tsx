05f3a65d931c479384631dacce007fa453aa6549
Axel Velasquez <yoloax11@gmail.com>
2025-02-18 20:24:10 -0800

    modified the app to include the nav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Schedule from './pages/Schedule'
+import NavBar from './components/Navbar'
+import SettingsPage from './pages/Settings'
 
 function App() {
 
 
   return (
     <>
+
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/schedule" element={<Schedule />} />
+          <Route path="/settings" element={<SettingsPage />} />
         </Routes>
       </BrowserRouter>
     </>
