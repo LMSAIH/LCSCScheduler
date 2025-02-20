386d29239eb739d3e275509db167b0fca6130f5f
Axel Velasquez <yoloax11@gmail.com>
2025-02-19 17:25:04 -0800

    added the authentication to the application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,30 @@
+import { useAuthContext } from './context/AuthContext'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Schedule from './pages/Schedule'
 import NavBar from './components/Navbar'
 import SettingsPage from './pages/Settings'
-
+import LoginPage from './pages/Login'
+import SignupPage from './pages/Signup'
+import NotFound from './pages/NotFound'
 function App() {
+
+  const {user} = useAuthContext();
 
 
   return (
     <>
 
       <BrowserRouter>
-        <NavBar />
+        {user && <NavBar />}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/" element={user ? <Home /> : <LoginPage />} />
+          <Route path="/schedule" element={user ? <Schedule /> : <LoginPage />} />
+          <Route path="/settings" element={user ? <SettingsPage /> : <LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
