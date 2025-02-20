99604da5abd2cbddc0a254b8ea19e668aebc0a6d
Axel Velasquez <yoloax11@gmail.com>
2025-02-19 17:25:22 -0800

    updated the main to have the context providers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { DarkModeProvider } from './context/DarkModeContext.tsx'
+import { AuthProvider } from './context/AuthContext.tsx'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
+    </AuthProvider>
   </StrictMode>,
 )
