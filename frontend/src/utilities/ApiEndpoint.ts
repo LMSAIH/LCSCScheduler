082fe06c6973cec11b420e9b9641c9b89d0093f1
Axel Velasquez <yoloax11@gmail.com>
2025-02-21 17:09:22 -0800

    updated the endpoint constants

--- frontend/src/utilities/ApiEndpoint.ts
+++ frontend/src/utilities/ApiEndpoint.ts
@@ -1,2 +1,5 @@
 
-export const APIBASEURL:string = 'http://localhost:8000/'
+export const APIBASEURL:string = 'http://localhost:8000'
+export const AUTHPREFIX:string = "/auth"
+export const SCHEDULEPREFIX:string = "/schedule"
+export const ADMINPREFIX:string = '/admin'
