07ce312279c6232558b13c5291d33c82a9892372
Axel Velasquez <yoloax11@gmail.com>
2025-02-21 17:08:35 -0800

    integrated the api into the app

--- frontend/src/components/Scheduler.tsx
+++ frontend/src/components/Scheduler.tsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios'
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { APIBASEURL, SCHEDULEPREFIX } from "../utilities/ApiEndpoint";
+import { useAuthContext } from "../context/AuthContext";
+import { v4 as uuidv4 } from 'uuid'
+import { TrashIcon } from "lucide-react";
+import {DateTime} from 'luxon'
+
 
 type Event = {
     id: string;
@@ -20,11 +27,56 @@ type Event = {
 export default function Scheduler() {
     const [events, setEvents] = useState<Event[]>([]);
     const [objectState, setObjectState] = useState<"Permanent" | "Temporary">("Permanent");
-    const [id, setId] = useState(1);
+    const { token } = useAuthContext();
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            try {
+                const response = await axios.get(`${APIBASEURL}${SCHEDULEPREFIX}/`, {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+
+                console.log(response.data)
+                setEvents(response.data);
+
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchData();
+    }, [token]);
+
+    const handleSend = async () => {
+        try {
+            const response = await axios.post(
+                `${APIBASEURL}${SCHEDULEPREFIX}/`,
+                events,
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleReset = async () => {
+        try {
+            const response = await axios.post(
+                `${APIBASEURL}${SCHEDULEPREFIX}/`,
+                [],
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     const handleEventClick = (info: any) => {
         if (window.confirm(`Delete event "${info.event.title}"?`)) {
-
             setEvents(events.filter(event => event.id !== info.event.id));
         }
     };
@@ -34,29 +86,34 @@ export default function Scheduler() {
             return;
         }
 
+        console.log(info)
+
         let eventStart: Date, eventEnd: Date;
 
         if (info.allDay) {
-            eventStart = new Date(info.start);
+            eventStart = new Date(info.end);
             eventStart.setHours(7, 0, 0, 0);
-            eventEnd = new Date(info.start);
+            eventEnd = new Date(info.end);
             eventEnd.setHours(19, 0, 0, 0);
         } else {
-            if (info.start.getDay() !== info.end.getDay()) {
-                document.body.style.cursor = "not-allowed";
-                setTimeout(() => {
-                    document.body.style.cursor = "";
-                }, 300);
-                return;
+            eventStart = new Date(info.startStr);
+            eventEnd = new Date(info.endStr);
+        
+            if (eventStart.getDay() !== eventEnd.getDay()) {
+              document.body.style.cursor = "not-allowed";
+              setTimeout(() => {
+                document.body.style.cursor = "";
+              }, 300);
+              return;
             }
-            eventStart = info.start;
-            eventEnd = info.end;
+            console.log(eventStart, eventEnd);
         }
 
         let newEvent: Event;
         if (objectState === "Permanent") {
+            
             newEvent = {
-                id: String(id),
+                id: uuidv4(),
                 title: "Permanent",
                 daysOfWeek: [eventStart.getDay()],
                 startTime: eventStart.toTimeString().slice(0, 8),
@@ -64,21 +121,22 @@ export default function Scheduler() {
                 color: "green",
                 type: objectState,
             };
+            console.log("Here new event:", newEvent)
         } else {
+            
             newEvent = {
-                id: String(id),
+                id: uuidv4(),
                 title: "Temporary",
-                start: eventStart,
-                end: eventEnd,
+                start: info.start,
+                end: info.end,
                 color: "yellow",
                 type: objectState,
             };
         }
 
-        setEvents([...events, newEvent]);
-
+        setEvents((prevEvents) => [...prevEvents, newEvent]);
         console.log(events)
-        setId(id + 1);
+
     };
 
     const handleEventDrop = (info: any) => {
@@ -86,6 +144,7 @@ export default function Scheduler() {
             info.revert();
             return;
         }
+
         const updatedEvents = events.map((event) => {
 
 
@@ -98,13 +157,19 @@ export default function Scheduler() {
                         end: info.event.end,
                     };
                 } else {
-                    
+
+                    //TODO: Make this work properly
+                    const vancouverStart = DateTime.fromJSDate(info.event.start).setZone("America/Vancouver");
+                    const vancouverEnd = DateTime.fromJSDate(info.event.end).setZone("America/Vancouver").toFormat("HH:mm:ss");
+
+                    console.log(vancouverStart,vancouverEnd);
+            
                     return {
-                        ...event,
-                        daysOfWeek: [info.event.start.getDay()],
-                        startTime: info.event.start.toTimeString().slice(0,8),
-                        endTime: info.event.end.toTimeString().slice(0,8)
-                    }
+                      ...event,
+                      daysOfWeek: [vancouverStart.weekday % 7],
+                      startTime: vancouverStart.toFormat("HH:mm:ss"),
+                      endTime: vancouverEnd
+                    };
                 }
             }
             return event;
@@ -129,6 +194,7 @@ export default function Scheduler() {
                 </button>
             </div>
             <FullCalendar
+                timeZone="America/Vancouver"
                 plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
                 initialView="timeGridWeek"
                 editable
@@ -162,8 +228,18 @@ export default function Scheduler() {
                 eventLongPressDelay={200}
                 selectLongPressDelay={100}
                 aspectRatio={1}
-                
+
             />
+
+            <div className="mt-8 flex justify-center flex-row gap-x-5">
+                <button onClick={handleSend}
+                    className="px-6 py-2 bg-[#F15A29] hover:bg-[#D14918] 
+                       rounded-md transition-colors font-medium"
+                >
+                    Send Schedule
+                </button>
+                <TrashIcon className="my-auto hover:text-red-500 duration-300" onClick={handleReset} />
+            </div>
         </div>
     );
 };
