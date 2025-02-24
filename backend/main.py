0574beff16f2c1a1b9a11330f20c75d745fd3dbe
Axel Velasquez <yoloax11@gmail.com>
2025-02-23 17:03:39 -0800

    added the roles routes

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from routes.v1.auth import router as auth_router
 from routes.v1.schedule import router as schedule_router
 from routes.v1.admin import router as admin_router
+from routes.v1.roles import router as roles_router
 
 app = FastAPI()
 
@@ -25,3 +26,4 @@ async def root():
 app.include_router(auth_router, prefix="/auth")
 app.include_router(schedule_router, prefix="/schedule")
 app.include_router(admin_router, prefix="/admin")
+app.include_router(roles_router, prefix="/roles")
