c9cd7dcf1ab0a37123ce47560c16e8f2e3c192ff
stahsin00 <lamisa1998@hotmail.com>
2025-02-25 14:58:09 -0800

    admin route

--- backend/routes/v1/admin.py
+++ backend/routes/v1/admin.py
@@ -24,13 +24,46 @@ def get_availabilities(supabase: Client = Depends(get_supabase)):# -> List[Avail
         prev_sunday = beginning_of_week.isoformat()
         next_saturday = end_of_week.isoformat()
 
-        query = supabase.table("events").select("*, auth.users(*)").or_(f"start_date.is.null, and(start_date.gte.'{prev_sunday}', start_date.lte.'{next_saturday}')").execute()
+        events_query = supabase.table("events").select("*, profiles(*)").or_(f"start_date.is.null, and(start_date.gte.'{prev_sunday}', start_date.lte.'{next_saturday}')").execute()
+        events = events_query.data
         
-        # for i in range(7):
-        #     for j in range(24):
-        #         print("hi")
+        availability_slots = []
+        for i in range(7): # TODO: use hashmap
+            current_day = beginning_of_week + timedelta(days=i)
+            for j in range(23):
+                slot_start = current_day.replace(hour=j, minute=0, second=0, microsecond=0)
+                slot_end = current_day.replace(hour=j + 1, minute=0, second=0, microsecond=0)
+                count = 0
 
-        return query
+                for event in events:
+                    if event.get("event_type") == "Permanent" and event["start_time"] and event["end_time"] and event["day_of_week"]:
+                        event_day = beginning_of_week + timedelta(days=event["day_of_week"])
+
+                        event_start_time = datetime.strptime(event["start_time"], "%H:%M:%S").time()
+                        event_end_time = datetime.strptime(event["end_time"], "%H:%M:%S").time()
+
+                        event_start = PST.localize(datetime.combine(event_day.date(), event_start_time))
+                        event_end = PST.localize(datetime.combine(event_day.date(), event_end_time))
+
+                    elif event["event_type"] == "Temporary" and event["start_date"] and event["end_date"]:
+                        event_start = PST.localize(datetime.fromisoformat(event["start_date"]))
+                        event_end = PST.localize(datetime.fromisoformat(event["end_date"]))
+
+                    else:
+                        continue
+
+                    if event_end > slot_start and event_start < slot_end:
+                        count += 1
+
+                availability_slots.append({
+                    "startDate": slot_start,
+                    "endDate": slot_end,
+                    "numberOfPeople": count,
+                    "maxPeopleAvailable": 10,
+                    "role": "All"
+                })
+
+        return availability_slots
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error getting availabilities: {str(e)}")
